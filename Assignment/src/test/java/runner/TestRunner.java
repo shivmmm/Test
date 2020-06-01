febcd4b1b99d2c28658b2b155b61eef505f246c4
shivmmm <31877665+shivmmm@users.noreply.github.com>
2020-06-01 14:47:22 +0530

    Update TestRunner.java
    Changes in tags

--- Assignment/src/test/java/runner/TestRunner.java
+++ Assignment/src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "resources/featureFiles", glue = "stepDefination", tags = { "@Regression" },
+@CucumberOptions(features = "resources/featureFiles", glue = "stepDefination", tags = { "@Regression,@Acceptance" },
 
 		plugin = { "pretty", "html:target/html/", })
 public class TestRunner {
